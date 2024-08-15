76d8ed2c1702de4dcb1d74dd50f339c87bd981f8
Lucas Souza <lsouza.dev@outlook.com>
2024-08-14 21:18:02 -0300

    Projeto para Viagens

---
+++ Journey/Journey/src/Journey.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using Journey.Communication.Responses;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Journey.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    void IExceptionFilter.OnException(ExceptionContext context)
+    {
+        if(context.Exception is JourneyException)
+        {
+            var JourneyException = (JourneyException)context.Exception;
+
+            context.HttpContext.Response.StatusCode = (int)JourneyException.GetStatusCode();
+
+            var responseJson = new ResponseErrorsJson(JourneyException.GetErrorMessages());
+
+            context.Result = new ObjectResult(responseJson);
+        }
+        else
+        {
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var list = new List<string>
+            {
+                "Erro desconhecido"
+            };
+
+            var responseJson = new ResponseErrorsJson(list); 
+
+            context.Result = new ObjectResult(responseJson);
+        }
+    }
+}
